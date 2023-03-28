4e60702cf83079e744bd438513f080156f5e5e24
Aribunfi <arianna@pabis.de>
2023-03-28 16:47:22 +0200

    initial commit - fix Alessio

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Blade</title>
+</head>
+<body>
+
+    <h1>Hello World!</h1>
+    <img src="https://cdn.dribbble.com/users/247591/screenshots/14092578/media/df7223803c3ac89fb17304c8cc9c8d5b.png?compress=1&resize=400x300" alt="">
+    
+</body>
+</html>
